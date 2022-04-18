33c019a2fc20cb0d1dcc85231495ff41b3ccf0eb
MahmodHamed <mahmod.hamed.1996.m@gmail.com>
2022-04-18 23:03:27 +0300

    sigup

--- FE/src/app/authentication/signup/signup.component.ts
+++ FE/src/app/authentication/signup/signup.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Customer } from '../../beans/customer';
+import { CustomerService } from '../../services/customer.service';
+import  Swal from 'sweetalert2';
+
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -12,10 +17,13 @@ export class SignupComponent implements OnInit {
   returnUrl: string;
   hide = true;
   chide = true;
+  newCustomer: Customer=new Customer();
+
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private customerService:CustomerService
   ) {}
   ngOnInit() {
     this.authForm = this.formBuilder.group({
@@ -25,7 +33,9 @@ export class SignupComponent implements OnInit {
         [Validators.required, Validators.email, Validators.minLength(5)],
       ],
       password: ['', Validators.required],
-      cpassword: ['', Validators.required],
+      phone: ['', Validators.required],
+      address: ['', Validators.required],
+
     });
     // get return url from route parameters or default to '/'
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
@@ -41,5 +51,36 @@ export class SignupComponent implements OnInit {
     } else {
       this.router.navigate(['/admin/dashboard/main']);
     }
+  }
+
+  register(){
+    this.customerService.addCustomer(this.newCustomer).subscribe(
+      res=>{
+        if(res){
+        
+          
+          Swal.fire({
+            icon: 'success',
+            title: 'נרשמתה',
+            text: 'נרשמתה בהצלחה ',
+
+        }
+
+        
+        );
+        this.router.navigate(['/admin/dashboard/main'])
+        }
+        
+        if(!res){
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'המשתמש זה קיים',
+
+        });
+        }
+
+      }
+    )
   }
 }

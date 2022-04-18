bd201208081fa5bd05dc0893a39038cb4d651fa9
MahmodHamed <mahmod.hamed.1996.m@gmail.com>
2022-04-19 02:00:08 +0300

    new componenent screen container

--- FE/src/app/admin/admin.module.ts
+++ FE/src/app/admin/admin.module.ts
@@ -2,9 +2,59 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AllTechnicianComponent } from './all-technician/all-technician.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ComponentsModule } from '../shared/components/components.module';
+import { SharedModule } from '../shared/shared.module';
+import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MaterialFileInputModule } from 'ngx-material-file-input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ScreenContComponent } from '../screen-cont/screen-cont.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, AdminRoutingModule],
+  declarations: [
+    AllTechnicianComponent,
+    ScreenContComponent
+  ],
+  imports: [CommonModule,
+     AdminRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatSortModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    MatTabsModule,
+    MatMenuModule,
+    MaterialFileInputModule,
+    MatProgressSpinnerModule,
+    ComponentsModule,
+  
+   
+    ],
 })
 export class AdminModule {}
